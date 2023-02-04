0dd395b97e305f14be5fbcbc9467ca68e7bd125b
Karimli Rufat <RufatrK@code.local>
2023-02-04 12:49:55 +0400

    test

---
+++ 5 reqemli eded/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _5_reqemli_eded
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+            int tam = (int)number;
+            decimal kesr = number - tam;
+            kesr *= 1000000;
+            kesr += tam;
+            kesr /= 1000;
+            Console.WriteLine(kesr);
+        }
+    }
+}
